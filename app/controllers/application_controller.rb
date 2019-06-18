6047c0f326de610ace95850e528efdc6514a525e
NotedQuotient9 <matt.beanland@gmail.com>
2019-06-18 17:11:13 +0100

    added authentification for admins and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+	def current_admin
+		if session[:admin_id]
+			Admin.find_by(id: session[:admin_id])
+		end
+	end
+
+	def authenticate
+		unless current_admin != nil
+			flash[:danger] = "You must be logged in to view this"
+			redirect_to login_url
+		end
+	end
+
 end
