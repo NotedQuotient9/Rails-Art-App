cbc3c754411030935b1037bde78a45a1ad84d145
NotedQuotient9 <matt.beanland@gmail.com>
2019-06-18 17:40:07 +0100

    added non-logged in view

---
+++ app/controllers/general_controller.rb
@@ -0,0 +1,7 @@
+class GeneralController < ApplicationController
+  def index
+  	@posts = Post.all
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'general/index'
   get 'sessions/new'
   get 'admins/new'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
+  root to: "general#index"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
